9d693f108bbc683d3cac1493d0221216963c14d1
cfsepulveda <cf.sepulveda@uniandes.edu.co>
2019-02-03 23:21:04 -0500

    dev

--- agileProject/settings.py
+++ agileProject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h*lr3(ksoeyfrk(zs4#v$eeksid2jcl##-^tz4y*+0ujl#@zlz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
