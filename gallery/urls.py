6c699005bb8b23d6111bf5f75d8acf401014f903
VIVASOFT <vivasoft@varca-2.local>
2019-02-10 14:55:03 -0500

    url index viewimages

--- gallery/urls.py
+++ gallery/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
 	url(r'^login/$', views.login_view, name='login'),
 	url(r'^loginUser', views.login_user, name='loginUser'),
 	url(r'^logout/$', views.logout_view, name='logout'),
+	url(r'^viewImages', views.index, name='viewImages'),
 	path('imageDetails/<int:id>', views.image_details, name='imageDetails'),
 	path('audioDetails/<int:id>', views.audio_details, name='audioDetails'),
 	path('videoDetails/<int:id>', views.video_details, name='videoDetails'),	
